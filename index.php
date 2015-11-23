2848e6a0600442d64a73fd4a8118186b68cf52b9
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-11-23 13:12:27 +0200

    added jumbotron adn tabs

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <!-- ############### -->
     <!-- #### MENÜÜ #### -->
     <!-- ############### -->
-    <nav class="navbar navbar-default">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
       <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
@@ -25,11 +25,37 @@
         </div><!-- /.navbar-collapse -->
       </div><!-- /.container-fluid -->
     </nav>
-
-
-    <h1>Hello, world!</h1>
     
+    <br><br><br><br>
+
+    <!-- ############### -->
+    <!-- ##### SISU #### -->
+    <!-- ############### -->
+    <div class="container-fluid">
+        <div class="row">
+        
+            <div class="col-sm-6 col-sm-push-6">
+                <ul class="nav nav-tabs">
+                  <li role="presentation" class="active"><a href="#">Home</a></li>
+                  <li role="presentation"><a href="#">Profile</a></li>
+                  <li role="presentation"><a href="#">Messages</a></li>
+                </ul>
+            </div>
+        
+            <div class="col-sm-6 col-sm-pull-6">
+                <div class="jumbotron">
+                  <div class="container">
+                      <h1>Hello, world!</h1>
+                      <p>...</p>
+                      <p><a class="btn btn-success pull-right" href="#" role="button">Learn more</a></p>
+                  </div>
+                </div>
+            </div>
+            
+            
     
+        </div>
+    </div>
     
     
 <?php require_once("footer.php"); ?>
